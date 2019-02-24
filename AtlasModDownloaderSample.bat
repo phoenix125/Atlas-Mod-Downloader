55b5e7de786cbcebd8b595c92d9370aab849572b
Phoenix125 <kim@kim125.com>
2019-02-24 13:58:25 -0500

    Update AtlasModDownloaderSample.bat

--- AtlasModDownloaderSample.bat
+++ AtlasModDownloaderSample.bat
@@ -1,3 +1,3 @@
 @echo off
-AtlasModDownloader_v1.0.exe --modids 1635287345 --workingdir D:\temp --steamcmd "D:\Game Servers\SteamCMD" --workingdir "D:\Game Servers\Atlas Dedicated Server"
+AtlasModDownloader_v1.0.exe --modids 1635287345 --steamcmd "D:\Game Servers\SteamCMD" --workingdir "D:\Game Servers\Atlas Dedicated Server"
 @pause
